9dd88190d8c96d745c499dd34b5be7019ac6b4b9
Denis <den.sanches@mail.ru>
2020-01-10 18:37:23 +0300

    shopno

---
+++ file.js
@@ -0,0 +1,15 @@
+// document.getElementById("item1").onmouseover = function(){
+//     openCloseMenu()
+// }
+
+// function openCloseMenu(){
+//     document.getElementById("i01").classList.toggle("open");
+// }
+
+document.getElementById("image1").onclick = function(){
+    openCloseMenu()
+}
+
+function openCloseMenu(){
+    document.getElementById("our1").classList.toggle(open)
+}
